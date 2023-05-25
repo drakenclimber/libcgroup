e69d867fb69ecca88683c89361f0d69c7b4296f5
Tom Hromatka <tom.hromatka@oracle.com>
2023-05-24 20:35:35 -0600

    python: Add method to clear the default slice/scope
    Add a convenience method to clear the default slice/scope.  This may be
    useful in cases where the user is done with the slice/scope and wants to
    resume operating at the root cgroup level.
    
    Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

--- src/python/libcgroup.pyx
+++ src/python/libcgroup.pyx
@@ -615,6 +615,25 @@ cdef class Cgroup:
         if set_default:
             Cgroup.__set_default_systemd_cgroup()
 
+    @staticmethod
+    def clear_default_systemd_scope():
+        """Clear/Remove the default slice/scope from libcgroup
+
+        Description:
+        Delete the default slice/scope from the libcgroup var/run file.
+        Also deletes the internal static variable within libcgroup, so that
+        the default path is restored back to the root cgroup
+        """
+        try:
+            Cgroup.write_default_systemd_scope('', '')
+        except RuntimeError:
+            pass
+
+        try:
+            Cgroup.__set_default_systemd_cgroup()
+        except RuntimeError:
+            pass
+
     cdef compare(self, Cgroup other):
         """Compare this cgroup instance with another cgroup instance
 
